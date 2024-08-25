98cb0074fe1da44e85640300773a7bc3de13d767
vinitcs <vinitchavan1234@gmail.com>
2024-08-25 12:07:52 +0530

    feat: add row, add col, cell border and input added

--- app/components/Table.js
+++ app/components/Table.js
@@ -1,21 +1,21 @@
-"use client"
+"use client";
 
 import { useState } from "react";
 
 const Table = ({ initialGrid }) => {
-    const [grid, setGrid] = useState(initialGrid)
+  const [grid, setGrid] = useState(initialGrid);
 
-    const handleCellChange = (rowIndex, colIndex, cellValue) => {
-        const newGrid = [...initialGrid]
-        newGrid[rowIndex, colIndex] = cellValue;
-        setGrid(newGrid);
-    }
-    console.log(initialGrid)
-    return (
-        <>
-        <div className="p-2">
-            <table className="table-auto overflow-auto w-full ">
-                {/* <thead>
+  const handleCellChange = (rowIndex, colIndex, cellValue) => {
+    const newGrid = [...initialGrid];
+    newGrid[(rowIndex, colIndex)] = cellValue;
+    setGrid(newGrid);
+  };
+  console.log(initialGrid);
+  return (
+    <>
+      <div className="w-full h-[25.8rem] overflow-auto p-2">
+        <table className="table-auto overflow-auto w-full ">
+          {/* <thead>
                     <tr>
                         <th>firest</th>
                         <th>firest</th>
@@ -23,27 +23,31 @@ const Table = ({ initialGrid }) => {
                         <th>firest</th>
                     </tr>
                 </thead> */}
-                <tbody>
-                    {
-                        initialGrid.map((curRow, rowIndex) => (
-                            <tr key={rowIndex}>
-                                {
-                                    curRow.map((curCell, colIndex) => (
-                                        <td key={colIndex}
-                                            className="bg-red"
-                                        // onChange={() => handleCellChange(rowIndex, colIndex, e.target.value)}
-                                        >
-                                            {curCell.value || "hi"}</td>
-                                    ))
-                                }
-                            </tr>
-                        ))
-                    }
-                </tbody>
-            </table>
-        </div>
-        </>
-    )
-}
+          <tbody>
+            {grid.map((curRow, rowIndex) => (
+              <tr key={rowIndex}>
+                {curRow.map((curCell, colIndex) => (
+                  <td
+                    key={colIndex}
+                    className="text-slate-800 text-center outline outline-1 "
+                  >
+                    <input
+                      type="text"
+                      value={curCell.value}
+                      className="w-full h-full text-center bg-transparent border-none outline-none"
+                      onChange={(e) =>
+                        handleCellChange(rowIndex, colIndex, e.target.value)
+                      }
+                    />
+                  </td>
+                ))}
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+    </>
+  );
+};
 
 export default Table;
--- app/page.js
+++ app/page.js
@@ -8,22 +8,24 @@ import {
   FaPlus,
   FaMinus,
 } from "react-icons/fa6";
+import { IoIosAdd } from "react-icons/io";
 
 import Table from "./components/Table";
 
-
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center p-10 bg-slate-800">
-      <div className="bg-slate-100 flex-col  w-full h-screen justify-center rounded-md relative">
+    <main className="flex h-screen flex-col items-center p-10 bg-slate-800">
+      <div className="bg-slate-100 flex-col  w-full h-full justify-center rounded-md relative">
         {/* Features Section */}
-        <div className="px-3 py-1 @media bg-red-400 w-full h-auto rounded-r-md  rounded-b-md items-center flex flex-wrap gap-3 ">
-          <h1 className="bg-red-400 rounded-t text-lg px-2 font-mono text-center font-extrabold tracking-wide cursor-default select-none absolute left-0 -top-7 text-slate-800">MS Excel App</h1>
+        <div className="px-3 py-1 @media bg-slate-200 w-full h-auto rounded-r-md  rounded-b-md items-center flex flex-wrap gap-3 ">
+          <h1 className="bg-red-400 rounded-t text-lg px-2 font-mono text-center font-extrabold tracking-wide cursor-default select-none absolute left-0 -top-7 text-slate-800">
+            MS Excel App
+          </h1>
 
           {/* Undo Redo */}
           <div className="w-max h-10 justify-center items-center flex gap-3">
-            <LuUndo2 className="cursor-pointer text-black hover:text-white" />
-            <LuRedo2 className="cursor-pointer text-black hover:text-white" />
+            <LuUndo2 className="cursor-pointer text-black hover:text-red-500" />
+            <LuRedo2 className="cursor-pointer text-black hover:text-red-500" />
           </div>
 
           {/* Search/Filter */}
@@ -36,9 +38,9 @@ export default function Home() {
 
           {/* Left Center Right */}
           <div className="flex gap-4 ml-6">
-            <FaAlignLeft className="cursor-pointer text-black hover:text-white " />
-            <FaAlignCenter className="cursor-pointer text-black hover:text-white" />
-            <FaAlignRight className="cursor-pointer text-black hover:text-white" />
+            <FaAlignLeft className="cursor-pointer text-black hover:text-red-500 " />
+            <FaAlignCenter className="cursor-pointer text-black hover:text-red-500" />
+            <FaAlignRight className="cursor-pointer text-black hover:text-red-500" />
           </div>
 
           {/* Fonts */}
@@ -59,7 +61,7 @@ export default function Home() {
 
           {/* Font Size */}
           <div className="flex gap-2 items-center justify-center ml-6">
-            <FaMinus className="cursor-pointer text-black hover:text-white size-3" />
+            <FaMinus className="cursor-pointer text-black hover:text-red-500 size-3" />
             <select className="p-1 rounded-md w-16 text-center text-sm text-black  bg-slate-100 hover:bg-white cursor-pointer">
               <option value="8">8</option>
               <option value="10">10</option>
@@ -69,10 +71,23 @@ export default function Home() {
               <option value="18">18</option>
               <option value="20">20</option>
             </select>
-            <FaPlus className="cursor-pointer text-black hover:text-white size-3" />
+            <FaPlus className="cursor-pointer text-black hover:text-red-500 size-3" />
+          </div>
+
+          <div className="flex gap-1 items-center justify-center ml-6">
+            <h1 className="text-sm text-slate-950 font-medium">Add Row</h1>
+            <IoIosAdd className="cursor-pointer text-black hover:text-red-500 size-5" />
+          </div>
+
+          <div className="flex gap-1 items-center justify-center ml-6">
+            <h1 className="text-sm text-slate-950 font-medium">Add Column</h1>
+            <IoIosAdd className="cursor-pointer text-black hover:text-red-500 size-5" />
           </div>
         </div>
-      <Table initialGrid={initialGrid} />
+
+        <div className="h-3/4">
+          <Table initialGrid={initialGrid} />
+        </div>
       </div>
     </main>
   );
