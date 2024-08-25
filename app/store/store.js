fb74055c9cbf130fe9b45ace7f43757e2d2975db
Ankit <ankitvika02@gmail.com>
2024-08-25 20:37:31 +0530

    pushing to remove conflict

--- app/store/store.js
+++ app/store/store.js
@@ -4,29 +4,72 @@ import { initialGrid } from "@/utils/initialGrid";
 const useStore = create((set) => ({
     selectedCell: {
         rowIndex: 0,
-        colIndex: 0
+        colIndex: 0,
     },
     grid: initialGrid,
+    undoStack: [],
+    redoStack: [],
 
     setSelectedCell: (selectedCell) => {
-        set((state) => ({ selectedCell: selectedCell }))
+        set((state) => ({ selectedCell: selectedCell }));
     },
+
     setGridData: (gridData) => {
-        set((state) => ({ grid: gridData }))
+        set((state) => ({
+            grid: gridData,
+        }));
     },
-    setFormatAtIndex:(rowIndex,colIndex,formatedStyle)=>{
-        set((state)=>{
+
+    setFormatAtIndex: (rowIndex, colIndex, formatedStyle) => {
+        set((state) => {
             const newGrid = state.grid.map((row, rIdx) =>
-                row.map((cell, cIdx) =>(
+                row.map((cell, cIdx) =>
                     rIdx === rowIndex && cIdx === colIndex
-                      ? {...cell,cellFormat:formatedStyle}
-                      : cell
+                        ? { ...cell, cellFormat: formatedStyle }
+                        : cell
                 )
-                )
-              )
-              return { grid: newGrid };
-        })
-    }
-}))
+            );
+            return {
+                grid: newGrid,
+                undoStack: [...state.undoStack, [...newGrid]],
+            };
+        });
+    },
+
+    undo: () => {
+        set((state) => {
+            console.log("kay re",state.undoStack)
+              if (state.undoStack.length === 0) {
+                return state;
+              }
+
+              const prevGrid = state.undoStack[state.undoStack.length - 1];
+              const newUndoStack = state.undoStack.slice(0, -1);
+              console.log("mast",prevGrid)
+              return {
+                grid: prevGrid,
+                undoStack: newUndoStack,
+                redoStack: [...state.redoStack, [...prevGrid]],
+              };
+        });
+    },
+
+    redo: () => {
+        set((state) => {
+            if (state.redoStack.length === 0) {
+                return state;
+            }
+
+            const nextGrid = state.redoStack[state.redoStack.length - 1];
+            const newRedoStack = state.redoStack.slice(0, -1);
+
+            return {
+                grid: nextGrid,
+                redoStack: newRedoStack,
+                undoStack: [...state.undoStack, [...nextGrid]],
+            };
+        });
+    },
+}));
 
 export default useStore;
